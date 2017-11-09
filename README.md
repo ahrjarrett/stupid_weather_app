6e41f45e7c9e219c2b3fb2382963e15e0d8cbabb
Andrew Jarrett <ahrjarrett@gmail.com>
2017-11-08 20:54:50 -0700

    wire axios and geolocation to make async api call

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Stupid Weather App
 
 freeCodeCamp project to display local weather.
+
+[On Heroku](https://stupid-weather-app.herokuapp.com/)
+
+[Heroku buildpack](https://elements.heroku.com/buildpacks/chadwilken/compile-react-app)
--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+import './App.css'
+
+const ROOT_URL = `https://fcc-weather-api.glitch.me/api/current?`
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.geo = window.navigator.geolocation
+  }
+
+  componentWillMount() {
+  }
+
+  componentDidMount() {
+    this.geo.getCurrentPosition(position => {
+      const { coords } = position
+      const API_CALL = `${ROOT_URL}lon=${coords.latitude}&lat=${coords.longitude}`
+      const weather = axios.get(API_CALL)
+        .then(({ data }) => console.log(data))
+      //console.log(weather.data)
+
+
+
+      //console.log('your current position is ')
+      //console.log(`Latitude : ${coords.latitude}`);
+      //console.log(`Longitude: ${coords.longitude}`);
+      //console.log(`More or less ${coords.accuracy} meters.`);
+    })
+  }
+
   render() {
+
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Stupid Weather App</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
